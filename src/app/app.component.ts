104128e36db55e6a5f439b2d1a5a31802897d32a
RhyanSPisoni <rhyanoficial14@gmail.com>
2022-06-10 11:56:39 -0300

    Studing

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  public todos: Todo[] = [];
+  constructor() {
+    this.todos.push(new Todo('Algo 1', true));
+    this.todos.push(new Todo('Algo 2', false));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  unMarkAsDone(todo: Todo) {
+    todo.done = false;
+  }
+}
---
+++ src/models/todo.model.ts
@@ -0,0 +1,6 @@
+export class Todo {
+    constructor(
+        public todo: String,
+        public done: boolean
+    ) {}
+}
